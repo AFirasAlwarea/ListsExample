6c9ef7c77766575cbb08675d17448025d2a8236b
AFirasAlwarea <firas.wira@gmail.com>
2017-10-30 22:17:46 +0100

    Adding comments Better program execution with do while Creating methods

--- ListsExample/Program.cs
+++ ListsExample/Program.cs
@@ -15,63 +15,115 @@ namespace ListsExample
             List<int> grades = new List<int>();
 
             // Let the user enter grades of the students
-            Console.WriteLine("Enter the students grades");
+            EnterGrades(names, grades);
 
+            // Printing all the students
+            PrintAllStudents(names, grades);
+
+
+            // Printing the average and the max grades
+            int avg = Avrg(grades);
+            int max = grades.Max();
+            Console.WriteLine("The average grade in your class is {0}\r\nThe highest grade is {1}"
+                , avg, max);
+            // Printing the best students
+            BestStuents(names, grades);
+
+            // Make the user select to add or remove a student, or exit
+
+            // A boolean to exit from the menu
+            bool getOut = false;
+            do
+            {
+                Console.WriteLine("What do you want to do (Enter a number from the list below):");
+                Console.WriteLine(" 1 - Enter new students \r\n 2 - Remove on student \r\n 3 - exit");
+                int choice = GetNumber();
+                switch (choice)
+                {
+                    // Let the user enter new students
+                    case 1:
+                        EnterNewStudents(names, grades);
+                        Console.WriteLine("The new list of student looks like");
+
+                        // Printing all the students after the addition of the new students
+                        PrintAllStudents(names, grades);
+
+                        Console.WriteLine("The new average is: {0}, and the new highest grade is {1}", Avrg(grades), grades.Max());
+
+                        BestStuents(names, grades);
+                        break;
+                    // Let the user remove one student
+                    case 2:
+                        bool removed = RemoveOneStudent(names, grades);
+
+                        if (removed)
+                        {
+                            Console.WriteLine("Your class after removal");
+                            PrintAllStudents(names, grades);
+
+                            Console.WriteLine("The new average grade in your class is {0}\r\nThe new highest grade is {1}"
+                            , Avrg(grades), grades.Max());
+                        }
+                        break;
+                    // Exit
+                    case 3:
+                        getOut = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please from 1 to 3 only");
+                        break;
+                }
+            } while (!getOut);
+        }
+
+        #region Enter Grades Method
+        /// <summary>
+        /// Enter the grades for the students
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="grades"></param>
+        private static void EnterGrades(List<string> names, List<int> grades)
+        {
+            Console.WriteLine("Enter the students grades");
             foreach (var item in names)
             {
                 Console.WriteLine(item);
                 int oneGrade = GetNumber();
                 grades.Add(oneGrade);
             }
-
-            PrintAllStudents(names, grades);
-
-
-            int avg = Avrg(grades);
-            int max = grades.Max();
-            Console.WriteLine("The average grade in your class is {0}\r\nThe highest grade is {1}"
-                , avg, max);
-
-            EnterNewStudents(names, grades);
-
-            Console.WriteLine("The new list of student looks like");
-            PrintAllStudents(names, grades);
-
-            Console.WriteLine("The new average is: {0}", Avrg(grades));
-
-            BestStuents(names, grades);
-
-            bool removed = RemoveOneStudent(names, grades);
-
-            if (removed)
-            {
-                Console.WriteLine("Your class after removal");
-                PrintAllStudents(names, grades);
-
-                Console.WriteLine("The new average grade in your class is {0}\r\nThe new highest grade is {1}"
-                , Avrg(grades), grades.Max());
-            }
-
-            Console.ReadKey();
         }
+        #endregion
 
+        #region Remove One Student
+        /// <summary>
+        /// Get a name of a student and remove it with is grade from the class
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="grades"></param>
+        /// <returns>Boolean that a student has been removed</returns>
         private static bool RemoveOneStudent(List<string> names, List<int> grades)
         {
+            // Boolean tells that a student has been removed
             bool removed = false;
+
+            // Ask the user if he wants to remove a student
             Console.WriteLine("Do you want to remove one of your students? (Y) for yes, any button else will exit");
             string remove = Console.ReadLine();
-
+            // Check if the user entered y that he wants to remove
             if (remove == "y" || remove == "Y")
             {
                 Console.WriteLine("Enter the name of the student that you want to remove");
                 string removeStudent = Console.ReadLine().ToLower();
-
+                // Check if the user did enter a valid string for a name
                 if (!string.IsNullOrEmpty(removeStudent))
                 {
+                    // Search for the name of the student
                     for (int i = 0; i < names.Count; i++)
                     {
+                        // There are better ways to check strings matching
                         if (removeStudent == names[i].ToLower())
                         {
+                            // Ask the user if he is sure to remove the student
                             Console.WriteLine("I found {0}. Are you sure you want to remove?", names[i]);
                             string sure = Console.ReadLine();
                             if (sure.Contains("y") || sure.Contains("Y"))
@@ -82,6 +134,7 @@ namespace ListsExample
                             }
                         }
                     }
+                    // If there is no student matching the name, so he is not removed and not found in the class
                     if (!removed)
                     {
                         Console.WriteLine("I didn't find {0} in your class", removeStudent);
@@ -94,7 +147,14 @@ namespace ListsExample
             }
             return removed;
         }
+        #endregion
 
+        #region Best Students
+        /// <summary>
+        /// Creating a list of best students
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="grades"></param>
         private static void BestStuents(List<string> names, List<int> grades)
         {
             List<string> bestStudents = new List<string>();
@@ -116,7 +176,14 @@ namespace ListsExample
                 Console.WriteLine(item);
             }
         }
+        #endregion
 
+        #region New Students
+        /// <summary>
+        /// Get a number of new students and add it to both lists of names and grades
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="grades"></param>
         private static void EnterNewStudents(List<string> names, List<int> grades)
         {
             Console.WriteLine("Enter the number of the students that you want to add");
@@ -132,7 +199,9 @@ namespace ListsExample
                 grades.Add(newGrade);
             }
         }
+        #endregion
 
+        #region Printing Names and Grades
         private static void PrintAllStudents(List<string> names, List<int> grades)
         {
             for (int i = 0; i < names.Count; i++)
@@ -140,14 +209,27 @@ namespace ListsExample
                 Console.WriteLine("Student {0} has a grade of {1}", names[i], grades[i]);
             }
         }
+        #endregion
 
+        #region Average Calculations
+        /// <summary>
+        /// Calculating the average grade in the class
+        /// </summary>
+        /// <param name="grades"></param>
+        /// <returns>integar of average</returns>
         private static int Avrg(List<int> grades)
         {
             int sum = grades.Sum();
             int avg = sum / grades.Count;
             return avg;
         }
+        #endregion
 
+        #region Getting Integer
+        /// <summary>
+        /// Let the user enter an integer and check it, handling errors
+        /// </summary>
+        /// <returns></returns>
         private static int GetNumber()
         {
             int number = 0;
@@ -176,5 +258,6 @@ namespace ListsExample
             } while (!succeeded);
             return number;
         }
+        #endregion
     }
 }
